600adfa1f94ace9a59b75a2588b794b27562a547
Alison8547 <alisonailson100@hotmail.com>
2023-09-26 10:52:34 -0300

    feat: Add Operation repository

---
+++ src/main/java/com/br/bank/repository/OperationRepository.java
@@ -0,0 +1,9 @@
+package com.br.bank.repository;
+
+import com.br.bank.entity.Operation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OperationRepository extends JpaRepository<Operation, Integer> {
+}
