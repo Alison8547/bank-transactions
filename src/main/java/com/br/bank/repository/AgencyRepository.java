6899c5c18f3d3c67433beb333def6adbe2e497b1
Alison8547 <alisonailson100@hotmail.com>
2023-10-01 13:31:43 -0300

    feat: Add AgencyService

--- src/main/java/com/br/bank/repository/AgencyRepository.java
+++ src/main/java/com/br/bank/repository/AgencyRepository.java
@@ -4,6 +4,10 @@ import com.br.bank.entity.Agency;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AgencyRepository extends JpaRepository<Agency, Integer> {
+
+    Optional<Agency> findByNumberAgency(Integer numberAgency);
 }
---
+++ src/main/java/com/br/bank/service/AgencyService.java
@@ -0,0 +1,20 @@
+package com.br.bank.service;
+
+import com.br.bank.entity.Agency;
+import com.br.bank.exception.BusinessException;
+import com.br.bank.repository.AgencyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AgencyService {
+
+    private final AgencyRepository agencyRepository;
+
+
+    public Agency findAgency(Integer numberAgency) {
+        return agencyRepository.findByNumberAgency(numberAgency)
+                .orElseThrow(() -> new BusinessException("Agency does not exist!"));
+    }
+}
