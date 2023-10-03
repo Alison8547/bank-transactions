8fca2023338ef9a0a5c4a8ea5d742fcbfca78d60
Alison8547 <alisonailson100@hotmail.com>
2023-10-03 12:39:33 -0300

    feat: jpa between timeOperation

--- src/main/java/com/br/bank/repository/OperationRepository.java
+++ src/main/java/com/br/bank/repository/OperationRepository.java
@@ -4,6 +4,12 @@ import com.br.bank.entity.Operation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface OperationRepository extends JpaRepository<Operation, Integer> {
+
+    Optional<List<Operation>> findAllByTimeOperationBetween(LocalDateTime start, LocalDateTime end);
 }
