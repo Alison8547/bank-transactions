224f28ca222c0c543f0310405cb0f6759f34cbe4
Alison8547 <alisonailson100@hotmail.com>
2023-10-07 13:51:38 -0300

    docs: Auth controller swagger

--- src/main/java/com/br/bank/controller/AuthController.java
+++ src/main/java/com/br/bank/controller/AuthController.java
@@ -2,6 +2,9 @@ package com.br.bank.controller;
 
 import com.br.bank.dto.request.LoginRequest;
 import com.br.bank.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +24,15 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "Authenticate your account", description = "Authenticate your database account")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Auth successfully"),
+                    @ApiResponse(responseCode = "400", description = "An exception was generated"),
+                    @ApiResponse(responseCode = "403", description = "You do not have permission to access this resource"),
+                    @ApiResponse(responseCode = "500", description = "An internal server exception was generated")
+            }
+    )
     @PostMapping("/auth")
     public ResponseEntity<String> auth(@Valid @RequestBody LoginRequest loginRequest) {
         return new ResponseEntity<>(authService.authentication(loginRequest), HttpStatus.OK);
